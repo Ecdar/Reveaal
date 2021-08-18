1fa1c90a14b912ff06508d3bc1e7997e5843c11b
Thorulf Neustrup <thorulf@live.dk>
2021-08-18 11:43:01 +0200

    Updated building system to use CMAKE (#71)
    * Updated building system to use CMAKE
    
    * Formatted build file

---
+++ dbm/toolchain-x86_64-w64-mingw32.cmake
@@ -0,0 +1,25 @@
+# the name of the target operating system
+set(CMAKE_SYSTEM_NAME Windows)
+set(TOOLCHAIN_PREFIX x86_64-w64-mingw32)
+
+#Used to define an output folder. The triple should correspond to the target triple that Reveaal is built to
+#For native systems leave empty
+set(triple x86_64-pc-windows-gnu)
+
+# which compilers to use for C and C++
+set(CMAKE_C_COMPILER ${TOOLCHAIN_PREFIX}-gcc)
+set(CMAKE_CXX_COMPILER ${TOOLCHAIN_PREFIX}-g++)
+set(CMAKE_AR ${TOOLCHAIN_PREFIX}-gcc-ar)
+set(CMAKE_RC_COMPILER ${TOOLCHAIN_PREFIX}-windres)
+set(CMAKE_STRIP ${TOOLCHAIN_PREFIX}-strip)
+set(CMAKE_SIZEOF_VOID_P 8) # somehow not set by default
+
+# here is the target environment located
+set(CMAKE_FIND_ROOT_PATH "${CMAKE_PREFIX_PATH}")
+
+# adjust the default behaviour of the FIND_XXX() commands:
+# search headers and libraries in the target environment,
+# search programs in both target and host environments
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM BOTH)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
