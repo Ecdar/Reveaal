b4e52c33980d431162bdf6f04c38f1a3d8c22ad7
Sebastian Lund <sebastianlund112@gmail.com>
2021-09-14 16:58:17 +0200

    Move dummy objects, encoding and decoding into seperate file

--- src/DataReader/mod.rs
+++ src/DataReader/mod.rs
@@ -3,4 +3,7 @@ pub mod json_writer;
 pub mod parse_edge;
 pub mod parse_invariant;
 pub mod parse_queries;
+pub mod serialization;
 pub mod xml_parser;
+
+use serialization::{DummyComponent, DummyEdge, DummyLocation};
