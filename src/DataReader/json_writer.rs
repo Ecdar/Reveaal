adf0133f15eac38f6b841121ac8acdd5ee7ebe55
Thorulf Neustrup <thorulf@live.dk>
2021-09-18 16:54:33 +0200

    Refactor of project loading (#74)
    * Added lazy loading with a ProjectLoader
    
    * Removed working directory dependency and cleanup
    
    * Rename of struct and cleanup
    
    * Solved clippy errors

--- src/DataReader/json_writer.rs
+++ src/DataReader/json_writer.rs
@@ -1,8 +1,13 @@
 use crate::ModelObjects::component::Component;
 use std::fs::File;
 
-pub fn component_to_json(component: &Component) {
-    let path = format!("Components/{}.json", component.get_name());
+pub fn component_to_json(project_path: &str, component: &Component) {
+    let path = format!(
+        "{0}{1}Components{1}{2}.json",
+        project_path,
+        std::path::MAIN_SEPARATOR,
+        component.get_name()
+    );
     let file = File::create(path).expect("Couldnt open file");
 
     serde_json::to_writer_pretty(&file, component).expect("Failed to serialize component");
