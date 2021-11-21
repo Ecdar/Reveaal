13262cac08c42fc2e27dce48696277f6384795a8
Thorulf Neustrup <thorulf@live.dk>
2021-11-21 19:58:52 +0100

    Refactored parse_invariant.rs

--- src/DataReader/parse_invariant.rs
+++ src/DataReader/parse_invariant.rs
@@ -1,7 +1,10 @@
 extern crate pest;
 use crate::ModelObjects::representations::BoolExpression;
-use pest::error::Error;
+use pest::iterators::Pair;
+use pest::iterators::Pairs;
 use pest::Parser;
+use simple_error::bail;
+use std::error::Error;
 
 /// This file handles parsing the invariants based on the abstract syntax described in the .pest files in the grammar folder
 /// For clarification see documentation on pest crate
@@ -9,122 +12,143 @@ use pest::Parser;
 #[grammar = "DataReader/grammars/invariant_grammar.pest"]
 pub struct InvariantParser;
 
-pub fn parse(edge_attribute_str: &str) -> Result<BoolExpression, Error<Rule>> {
-    let mut pairs = InvariantParser::parse(Rule::invariant, edge_attribute_str)
-        .unwrap_or_else(|e| panic!("Could not parse as rule with error: {}", e));
-    let pair = pairs.next().unwrap();
+pub fn parse(edge_attribute_str: &str) -> Result<BoolExpression, Box<dyn Error>> {
+    let mut pairs = InvariantParser::parse(Rule::invariant, edge_attribute_str)?;
+    let pair = try_next(&mut pairs)?;
     match pair.as_rule() {
-        Rule::invariant => Ok(build_invariant_from_pair(pair)),
+        Rule::invariant => build_invariant_from_pair(pair),
         err => {
-            panic!("Unable to match invariant string as rule: {:?}", err)
+            bail!("Unable to match invariant string as rule: {:?}", err)
         }
     }
 }
 
-pub fn build_invariant_from_pair(pair: pest::iterators::Pair<Rule>) -> BoolExpression {
-    let pair = pair.into_inner().next().unwrap();
+pub fn build_invariant_from_pair(
+    pair: pest::iterators::Pair<Rule>,
+) -> Result<BoolExpression, Box<dyn Error>> {
+    let pair = try_next(&mut pair.into_inner())?;
     match pair.as_rule() {
         Rule::andExpr => {
             let pair_span = pair.as_span();
 
             // check if we have an empty pair
             if pair_span.start() == pair_span.end() {
-                return BoolExpression::Bool(true);
+                return Ok(BoolExpression::Bool(true));
             }
 
             let mut inner_pairs = pair.into_inner();
-            let inner_pair = inner_pairs.next().unwrap();
+            let inner_pair = try_next(&mut inner_pairs)?;
 
             build_expression_from_pair(inner_pair)
         }
-        _ => panic!("Unable to match: {:?} as rule, guard", pair),
+        _ => bail!("Unable to match: {:?} as rule, guard", pair),
     }
 }
 
-fn build_expression_from_pair(pair: pest::iterators::Pair<Rule>) -> BoolExpression {
+fn build_expression_from_pair(
+    pair: pest::iterators::Pair<Rule>,
+) -> Result<BoolExpression, Box<dyn Error>> {
     match pair.as_rule() {
         Rule::term => build_term_from_pair(pair),
         Rule::parenthesizedExp => {
-            let inner_pair = pair.into_inner().next().unwrap();
-            BoolExpression::Parentheses(Box::new(build_expression_from_pair(inner_pair)))
+            let inner_pair = try_next(&mut pair.into_inner())?;
+            Ok(BoolExpression::Parentheses(Box::new(
+                build_expression_from_pair(inner_pair)?,
+            )))
         }
         Rule::andExpr => build_and_from_pair(pair),
         Rule::orExpr => build_or_from_pair(pair),
         Rule::compareExpr => build_compareExpr_from_pair(pair),
-        Rule::terms => build_expression_from_pair(pair.into_inner().next().unwrap()),
-        unknown => panic!("Got unknown pair: {:?}", unknown),
+        Rule::terms => build_expression_from_pair(try_next(&mut pair.into_inner())?),
+        unknown => bail!("Got unknown pair: {:?}", unknown),
     }
 }
 
-fn build_term_from_pair(pair: pest::iterators::Pair<Rule>) -> BoolExpression {
-    let inner_pair = pair.into_inner().next().unwrap();
+fn build_term_from_pair(
+    pair: pest::iterators::Pair<Rule>,
+) -> Result<BoolExpression, Box<dyn Error>> {
+    let inner_pair = try_next(&mut pair.into_inner())?;
     match inner_pair.as_rule() {
         Rule::atom => {
             if let Ok(n) = inner_pair.as_str().trim().parse::<bool>() {
-                BoolExpression::Bool(n)
+                Ok(BoolExpression::Bool(n))
             } else if let Ok(n) = inner_pair.as_str().trim().parse::<i32>() {
-                BoolExpression::Int(n)
+                Ok(BoolExpression::Int(n))
             } else {
                 build_term_from_pair(inner_pair)
             }
         }
-        Rule::variable => BoolExpression::VarName(inner_pair.as_str().trim().to_string()),
-        err => panic!("Unable to match: {:?} as rule atom or variable", err),
+        Rule::variable => Ok(BoolExpression::VarName(
+            inner_pair.as_str().trim().to_string(),
+        )),
+        err => bail!("Unable to match: {:?} as rule atom or variable", err),
     }
 }
 
-fn build_and_from_pair(pair: pest::iterators::Pair<Rule>) -> BoolExpression {
+fn build_and_from_pair(
+    pair: pest::iterators::Pair<Rule>,
+) -> Result<BoolExpression, Box<dyn Error>> {
     let mut inner_pair = pair.into_inner();
-    let left_side_pair = inner_pair.next().unwrap();
+    let left_side_pair = try_next(&mut inner_pair)?;
 
     match inner_pair.next() {
         None => build_or_from_pair(left_side_pair),
         Some(right_side_pair) => {
-            let lside = build_expression_from_pair(left_side_pair);
-            let rside = build_expression_from_pair(right_side_pair);
+            let lside = build_expression_from_pair(left_side_pair)?;
+            let rside = build_expression_from_pair(right_side_pair)?;
 
-            BoolExpression::AndOp(Box::new(lside), Box::new(rside))
+            Ok(BoolExpression::AndOp(Box::new(lside), Box::new(rside)))
         }
     }
 }
 
-fn build_or_from_pair(pair: pest::iterators::Pair<Rule>) -> BoolExpression {
+fn build_or_from_pair(pair: pest::iterators::Pair<Rule>) -> Result<BoolExpression, Box<dyn Error>> {
     let mut inner_pair = pair.into_inner();
-    let left_side_pair = inner_pair.next().unwrap();
+    let left_side_pair = try_next(&mut inner_pair)?;
 
     match inner_pair.next() {
         None => build_compareExpr_from_pair(left_side_pair),
         Some(right_side_pair) => {
-            let lside = build_expression_from_pair(left_side_pair);
-            let rside = build_expression_from_pair(right_side_pair);
+            let lside = build_expression_from_pair(left_side_pair)?;
+            let rside = build_expression_from_pair(right_side_pair)?;
 
-            BoolExpression::OrOp(Box::new(lside), Box::new(rside))
+            Ok(BoolExpression::OrOp(Box::new(lside), Box::new(rside)))
         }
     }
 }
 
-fn build_compareExpr_from_pair(pair: pest::iterators::Pair<Rule>) -> BoolExpression {
+fn build_compareExpr_from_pair(
+    pair: pest::iterators::Pair<Rule>,
+) -> Result<BoolExpression, Box<dyn Error>> {
     let mut inner_pair = pair.into_inner();
-    let left_side_pair = inner_pair.next().unwrap();
+    let left_side_pair = try_next(&mut inner_pair)?;
 
     match inner_pair.next() {
         None => build_expression_from_pair(left_side_pair),
         Some(operator_pair) => {
-            let right_side_pair = inner_pair.next().unwrap();
+            let right_side_pair = try_next(&mut inner_pair)?;
 
-            let lside = build_expression_from_pair(left_side_pair);
-            let rside = build_expression_from_pair(right_side_pair);
+            let lside = build_expression_from_pair(left_side_pair)?;
+            let rside = build_expression_from_pair(right_side_pair)?;
 
             match operator_pair.as_str() {
-                ">=" => BoolExpression::GreatEQ(Box::new(lside), Box::new(rside)),
-                "<=" => BoolExpression::LessEQ(Box::new(lside), Box::new(rside)),
-                "<" => BoolExpression::LessT(Box::new(lside), Box::new(rside)),
-                ">" => BoolExpression::GreatT(Box::new(lside), Box::new(rside)),
-                unknown_operator => panic!(
+                ">=" => Ok(BoolExpression::GreatEQ(Box::new(lside), Box::new(rside))),
+                "<=" => Ok(BoolExpression::LessEQ(Box::new(lside), Box::new(rside))),
+                "<" => Ok(BoolExpression::LessT(Box::new(lside), Box::new(rside))),
+                ">" => Ok(BoolExpression::GreatT(Box::new(lside), Box::new(rside))),
+                unknown_operator => bail!(
                     "Got unknown boolean operator: {}. Only able to match >=,<=,<,>",
                     unknown_operator
                 ),
             }
         }
+    }
+}
+
+fn try_next<'i>(iterator: &mut Pairs<'i, Rule>) -> Result<Pair<'i, Rule>, Box<dyn Error>> {
+    if let Some(pair) = iterator.next() {
+        Ok(pair)
+    } else {
+        bail!("Expected pair but got None instead")
     }
 }
