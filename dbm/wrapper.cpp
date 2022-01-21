42ff83ee5034d8dbda34c9f985699658f6fe0251
Thorulf Neustrup <thorulf@live.dk>
2022-01-21 12:54:05 +0100

    Removed memory leak

--- dbm/wrapper.cpp
+++ dbm/wrapper.cpp
@@ -4,29 +4,27 @@
 #include "dbm/dbm.h"
 #include "hash/compute.h"
 
-
-
 extern "C"
 {
-raw_t dbm_boundbool2raw_wrapper(int32_t bound, bool isStrict)
-{
-    return (bound * 2) | (isStrict ^ 1);
-}
+    raw_t dbm_boundbool2raw_wrapper(int32_t bound, bool isStrict)
+    {
+        return (bound * 2) | (isStrict ^ 1);
+    }
 
-void dbm_zero_wrapper(raw_t* dbm, cindex_t dim)
-{
-    base_fill(dbm, dbm + (dim * dim), dbm_LE_ZERO);
-}
+    void dbm_zero_wrapper(raw_t *dbm, cindex_t dim)
+    {
+        base_fill(dbm, dbm + (dim * dim), dbm_LE_ZERO);
+    }
 
-int32_t dbm_raw2bound_wrapper(raw_t raw) { return (raw >> 1); }
-bool dbm_rawIsStrict_wrapper(raw_t raw) { return ((raw & 1) ^ dbm_WEAK); }
-bool dbm_satisfies_wrapper(const raw_t* dbm, cindex_t dim, cindex_t i, cindex_t j,
-                                 raw_t constraint)
-{
-    assert(dbm && dim && i < dim && j < dim && dim > 0);
-    return !(dbm[i * dim + j] > constraint &&             /* tightening ? */
-             dbm_negRaw(constraint) >= dbm[j * dim + i]); /* too tight ? */
-}
+    int32_t dbm_raw2bound_wrapper(raw_t raw) { return (raw >> 1); }
+    bool dbm_rawIsStrict_wrapper(raw_t raw) { return ((raw & 1) ^ dbm_WEAK); }
+    bool dbm_satisfies_wrapper(const raw_t *dbm, cindex_t dim, cindex_t i, cindex_t j,
+                               raw_t constraint)
+    {
+        assert(dbm && dim && i < dim && j < dim && dim > 0);
+        return !(dbm[i * dim + j] > constraint &&             /* tightening ? */
+                 dbm_negRaw(constraint) >= dbm[j * dim + i]); /* too tight ? */
+    }
 
     dbm::fed_t dbm_subtract1_exposed(const raw_t *arg1, const raw_t *arg2, cindex_t dim)
     {
@@ -64,7 +62,7 @@ bool dbm_satisfies_wrapper(const raw_t* dbm, cindex_t dim, cindex_t i, cindex_t 
 
     void dbm_vec_to_fed(raw_t *dbm[], cindex_t len, cindex_t dim, dbm::fed_t *fed_out)
     {
-        dbm::fed_t fed = (*new dbm::fed_t(dim));
+        dbm::fed_t fed(dim);
 
         for (int i = 0; i < len; i++)
         {
@@ -139,13 +137,13 @@ bool dbm_satisfies_wrapper(const raw_t* dbm, cindex_t dim, cindex_t i, cindex_t 
     void dbm_fed_minus_fed(raw_t *dbm1[], raw_t *dbm2[], cindex_t len1, cindex_t len2, cindex_t dim, dbm::fed_t *fed_out)
     {
 
-        dbm::fed_t fed1 = (*new dbm::fed_t(dim));
+        dbm::fed_t fed1(dim);
         for (int i = 0; i < len1; i++)
         {
             fed1.add(dbm1[i], dim);
         }
 
-        dbm::fed_t fed2 = (*new dbm::fed_t(dim));
+        dbm::fed_t fed2(dim);
         for (int i = 0; i < len2; i++)
         {
             fed2.add(dbm2[i], dim);
