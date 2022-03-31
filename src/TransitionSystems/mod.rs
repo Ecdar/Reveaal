b72ecc3f567d6da40ef9ff4525729c56e51f239a
Sebastian Lund <sebastianlund112@gmail.com>
2022-03-31 19:20:47 +0200

    Move pruned component to own file

--- src/TransitionSystems/mod.rs
+++ src/TransitionSystems/mod.rs
@@ -2,12 +2,13 @@
 pub mod common;
 pub mod composition;
 pub mod conjunction;
+pub mod pruned_component;
 pub mod quotient;
 pub mod transition_system;
 
 pub use composition::Composition;
 pub use conjunction::Conjunction;
-pub use conjunction::PrunedComponent;
+pub use pruned_component::PrunedComponent;
 pub use quotient::Quotient;
 pub use transition_system::{
     CompositionType, LocationTuple, TransitionSystem, TransitionSystemPtr,
