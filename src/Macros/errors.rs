92aa41d9198b83be74013cce2780ff40ebb2b7d8
Sebastian Lund <sebastianlund112@gmail.com>
2022-04-11 02:51:24 +0200

    Prettify stacktrace print

--- src/Macros/errors.rs
+++ src/Macros/errors.rs
@@ -5,17 +5,24 @@ pub fn _add_info_to_result<T, E, R: Context<T, E>>(res: R, info: String) -> Resu
     res.with_context(|| info)
 }
 
+#[macro_export]
+macro_rules! location {
+    () => {
+        concat!(file!(), ":", line!(), ":", column!())
+    };
+}
+
 /// Add file and line number information + optional context to an Option<T> or an anyhow::Result<T, E>
 #[macro_export]
 macro_rules! context {
     ($result:expr) => { context!($result, "Unexpected error") };
-    ($result:expr, $($args:expr ),*) => { $crate::Macros::errors::_add_info_to_result($result, format!("{}: {}", concat!(file!(), ":", line!(), ":", column!()) ,format!( $( $args ),* ))) };
+    ($result:expr, $($args:expr ),*) => { $crate::Macros::errors::_add_info_to_result($result, format!("{}\n\t at {}", format!( $( $args ),* ), crate::location!())) };
 }
 
 /// Construct an anyhow::Error with file and line number information
 #[macro_export]
 macro_rules! error {
-    ($($args:expr ),*) => {$crate::anyhow::Error::msg(format!("{}: {}", concat!(file!(), ":", line!(), ":", column!()) ,format!( $( $args ),* )))}
+    ($($args:expr ),*) => {$crate::anyhow::Error::msg(format!("{}\n\t at {}", format!( $( $args ),* ), crate::location!()))}
 }
 
 /// Convert any Result into an anyhow::Result and add file and line number information + optional context
@@ -38,5 +45,5 @@ macro_rules! open {
 /// Bail with a result with file and line number information + optional context
 #[macro_export]
 macro_rules! bail {
-    ($($args:expr ),*) => {$crate::anyhow::bail!(format!("{}: {}", concat!(file!(), ":", line!(), ":", column!()) ,format!( $( $args ),* )))}
+    ($($args:expr ),*) => {$crate::anyhow::bail!(format!("{}\n\t at {}", format!( $( $args ),* ), crate::location!()))}
 }
