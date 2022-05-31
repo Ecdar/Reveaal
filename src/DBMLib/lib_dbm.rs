29c1855d406a4121179e53e09bd475ea0fd353fd
Sebastian Lund <sebastianlund112@gmail.com>
2022-05-31 02:48:36 +0200

    Hide warnings from unused FFI functions

--- src/DBMLib/lib_dbm.rs
+++ src/DBMLib/lib_dbm.rs
@@ -21,6 +21,7 @@ mod UDBM {
     #![allow(non_camel_case_types)]
     #![allow(dead_code)]
     #![allow(improper_ctypes)]
+    #![allow(deref_nullptr)]
     include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 }
 
