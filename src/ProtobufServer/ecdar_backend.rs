fdc1ca75ed0b49d30501c903dc8929fadb75a730
Sebastian Lund <sebastianlund112@gmail.com>
2022-06-14 17:06:06 +0200

    Change Status::unknown to Status::internal as that seems more appropriate from the tonic documentation

--- src/ProtobufServer/ecdar_backend.rs
+++ src/ProtobufServer/ecdar_backend.rs
@@ -41,7 +41,7 @@ where
 
     return match future.catch_unwind().await {
         Ok(response) => response,
-        Err(e) => Err(Status::unknown(format!(
+        Err(e) => Err(Status::internal(format!(
             "{}, please report this bug to the developers",
             downcast_to_string(e)
         ))),
