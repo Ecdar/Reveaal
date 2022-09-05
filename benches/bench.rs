41d7b57d07aba64ca14c00002703d0ca6eb21902
Sebastian Lund <sebastianlund112@gmail.com>
2022-09-05 20:44:33 +0200

    Remove unused imports

--- benches/bench.rs
+++ benches/bench.rs
@@ -1,5 +1,4 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use rand::{rngs::StdRng, Rng, SeedableRng};
+use criterion::{criterion_group, criterion_main, Criterion};
 use reveaal::tests::refinement::Helper::json_refinement_check;
 
 static PATH: &str = "samples/json/EcdarUniversity";
