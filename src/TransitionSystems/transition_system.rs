a86435ec04b39d4e755a4fb10d9d0a2fcd0ac646
Sigurd <j.skadborg00@gmail.com>
2022-10-04 15:34:48 +0200

    changed enum naming in consistency

--- src/TransitionSystems/transition_system.rs
+++ src/TransitionSystems/transition_system.rs
@@ -1,8 +1,5 @@
 use super::{CompositionType, LocationTuple};
-use crate::{
-    ModelObjects::component::{Declarations, State, Transition},
-    System::local_consistency::ConsistencyResult,
-};
+use crate::ModelObjects::component::{Declarations, State, Transition};
 use dyn_clone::{clone_trait_object, DynClone};
 use edbm::util::{bounds::Bounds, constraints::ClockIndex};
 use std::collections::hash_set::HashSet;
