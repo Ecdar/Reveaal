cafa73bda25f5625cb992cbda28cdd29cf857120
Casper-NS <caspernyvang@gmail.com>
2022-10-19 14:33:20 +0200

    Implemented a simple async query benchmark.

--- benches/bench.rs
+++ benches/bench.rs
@@ -1,5 +1,19 @@
-use criterion::{criterion_group, criterion_main, Criterion};
-use reveaal::tests::refinement::Helper::json_refinement_check;
+use std::vec;
+
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use reveaal::{
+    tests::refinement::Helper::json_refinement_check,
+    ProtobufServer::{
+        services::{
+            component::Rep, ecdar_backend_server::EcdarBackend, Component, ComponentsInfo,
+            QueryRequest,
+        },
+        ConcreteEcdarBackend,
+    },
+};
+use tonic::Request;
+
+use criterion::async_executor::FuturesExecutor;
 
 static PATH: &str = "samples/json/EcdarUniversity";
 
@@ -54,6 +68,46 @@ fn not_refinement(c: &mut Criterion) {
     bench_non_refinement(c, "Adm2 || Researcher <= Spec // Machine");
 }
 
-criterion_group!(benches, self_refinement, refinement, not_refinement);
+fn send_query_same_components(c: &mut Criterion) {
+    c.bench_function("send_query_same_components", |b| {
+        b.to_async(FuturesExecutor).iter(|| async {
+            let backend = ConcreteEcdarBackend::default();
+            let mut responses = vec![];
+            for _ in 0..64 {
+                let request = create_query_request("determinism: Machine");
+                responses.push(backend.send_query(request));
+            }
+
+            for response in responses {
+                _ = black_box(response.await);
+            }
+        });
+    });
+}
+
+fn create_query_request(query: &str) -> Request<QueryRequest> {
+    let json = std::fs::read_to_string(format!("{}/Components/Machine.json", PATH)).unwrap();
+
+    Request::new(QueryRequest {
+        user_id: 0,
+        query_id: 0,
+        query: String::from(query),
+        components_info: Some(ComponentsInfo {
+            components: vec![Component {
+                rep: Some(Rep::Json(json)),
+            }],
+            components_hash: 0,
+        }),
+        ignored_input_outputs: None,
+    })
+}
+
+criterion_group!(
+    benches,
+    self_refinement,
+    refinement,
+    not_refinement,
+    send_query_same_components
+);
 
 criterion_main!(benches);
