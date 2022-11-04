d19cefec2c073ae6388fe44e737961d4c4682dd9
claes <claesm1999@gmail.com>
2022-11-04 13:13:05 +0100

    test should be public :)

--- src/tests/failureMessageTests/mod.rs
+++ src/tests/failureMessageTests/mod.rs
@@ -1,4 +1,4 @@
-mod Consistency_test;
-mod Determinism_test;
-mod Refinement_enum_returns_correct_error_test;
+pub mod Consistency_test;
+pub mod Determinism_test;
+pub mod Refinement_enum_returns_correct_error_test;
 //pub mod tests::refinement::Helper;
