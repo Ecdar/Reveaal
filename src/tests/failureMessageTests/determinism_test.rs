e22eae61e775d09a1b75d4b5c9641c740834adb4
VictorDore <vdha20@student.aau.dk>
2022-11-05 01:43:49 +0100

    Revert "More formating" as formating is not legal
    This reverts commit ad83419433255ba4addf990652990bd575d4c437.

--- src/tests/failureMessageTests/determinism_test.rs
+++ src/tests/failureMessageTests/determinism_test.rs
@@ -13,21 +13,26 @@ mod test {
     fn determinism_failure_test() {
         let temp = json_run_query(PATH, "determinism: NonDeterminismCom");
         assert!(
-            if let QueryResult::Determinism(DeterminismResult::Failure(LocationID::Simple(_))) = temp
+            if let QueryResult::Determinism(DeterminismResult::Failure(LocationID::Simple(_))) =
+                temp
+            {
                 true
-            else
+            } else {
                 false
+            }
         );
     }
 
     #[test]
     fn determinism_failure_in_refinemnet_test() {
         let temp = json_run_query(PATH, "refinement: NonDeterminismCom <= Component2");
-        assert!(
-            if let QueryResult::Refinement(RefinementResult::Failure(RefinementFailure::DeterminismFailure(Some(_)),)) = temp
-                true
-            else
-                false
-        );
+        assert!(if let QueryResult::Refinement(RefinementResult::Failure(
+            RefinementFailure::DeterminismFailure(Some(_)),
+        )) = temp
+        {
+            true
+        } else {
+            false
+        });
     }
 }
--- src/tests/failureMessageTests/refinement_enum_returns_correct_error_test.rs
+++ src/tests/failureMessageTests/refinement_enum_returns_correct_error_test.rs
@@ -13,48 +13,56 @@ mod test {
     fn not_empty_result_test() {
         let temp = json_run_query(PATH, "refinement: A <= B");
 
-        assert!(
-            if let QueryResult::Refinement(RefinementResult::Failure(RefinementFailure::NotEmptyResult(_),)) = temp
-                true
-            else
-                false
-        );
+        assert!(if let QueryResult::Refinement(RefinementResult::Failure(
+            RefinementFailure::NotEmptyResult(_),
+        )) = temp
+        {
+            true
+        } else {
+            false
+        });
     }
 
     #[test]
     fn empty_transition2s_test() {
         let temp = json_run_query(PATH, "refinement: A <= A2");
 
-        assert!(
-            if let QueryResult::Refinement(RefinementResult::Failure(RefinementFailure::EmptyTransition2s(_),)) = temp
-                true
-            else
-                false
-        );
+        assert!(if let QueryResult::Refinement(RefinementResult::Failure(
+            RefinementFailure::EmptyTransition2s(_),
+        )) = temp
+        {
+            true
+        } else {
+            false
+        });
     }
 
     #[test]
     fn cuts_delay_solutions_test() {
         let temp = json_run_query(PATH, "refinement: A2 <= B2");
 
-        assert!(
-            if let QueryResult::Refinement(RefinementResult::Failure(RefinementFailure::CutsDelaySolutions(_),)) = temp
-                true
-            else
-                false
-        );
+        assert!(if let QueryResult::Refinement(RefinementResult::Failure(
+            RefinementFailure::CutsDelaySolutions(_),
+        )) = temp
+        {
+            true
+        } else {
+            false
+        });
     }
 
     #[test]
     fn initial_state_test() {
         let temp = json_run_query(PATH, "refinement: C <= D");
 
-        assert!(
-            if let QueryResult::Refinement(RefinementResult::Failure(RefinementFailure::InitialState(_),)) = temp
-                true
-            else
-                false
-        );
+        assert!(if let QueryResult::Refinement(RefinementResult::Failure(
+            RefinementFailure::InitialState(_),
+        )) = temp
+        {
+            true
+        } else {
+            false
+        });
     }
 
     #[test]
@@ -64,35 +72,41 @@ mod test {
             "refinement: notDisjointAndNotSubset1 <= notDisjointAndNotSubset2",
         );
 
-        assert!(
-            if let QueryResult::Refinement(RefinementResult::Failure(RefinementFailure::NotDisjointAndNotSubset,)) = temp
-                true
-            else
-                false
-        );
+        assert!(if let QueryResult::Refinement(RefinementResult::Failure(
+            RefinementFailure::NotDisjointAndNotSubset,
+        )) = temp
+        {
+            true
+        } else {
+            false
+        });
     }
 
     #[test]
     fn not_subset_test() {
         let temp = json_run_query(PATH, "refinement: notSubset1 <= notSubset2");
 
-        assert!(
-            if let QueryResult::Refinement(RefinementResult::Failure(RefinementFailure::NotSubset,)) = temp
-                true
-            else
-                false
-        );
+        assert!(if let QueryResult::Refinement(RefinementResult::Failure(
+            RefinementFailure::NotSubset,
+        )) = temp
+        {
+            true
+        } else {
+            false
+        });
     }
 
     #[test]
     fn not_disjoint_test() {
         let temp = json_run_query(PATH, "refinement: disJoint2 <= disJoint1");
 
-        assert!(
-            if let QueryResult::Refinement(RefinementResult::Failure(RefinementFailure::NotDisjoint,)) = temp
-                true
-            else
-                false
-        );
+        assert!(if let QueryResult::Refinement(RefinementResult::Failure(
+            RefinementFailure::NotDisjoint,
+        )) = temp
+        {
+            true
+        } else {
+            false
+        });
     }
 }
