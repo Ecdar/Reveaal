16a7e8509c43fa3bce3a29e4a0d7edd47ed33c90
Claes Berg Mortensen <claesm1999@gmail.com>
2022-11-15 22:23:02 +0100

    Added models and further tests

--- src/tests/failure_message/actions_test.rs
+++ src/tests/failure_message/actions_test.rs
@@ -1,6 +1,7 @@
 #[cfg(test)]
 
 mod test {
+    use crate::queries;
     use crate::tests::refinement::Helper::json_run_query;
     use crate::System::{
         executable_query::QueryResult,
@@ -8,16 +9,43 @@ mod test {
         refine::{RefinementFailure, RefinementResult},
     };
 
-    const PATH: &str = "samples/json/Determinism";
+    const PATH: &str = "samples/json/Actions";
 
     #[test]
-    fn determinism_test(){
-        let actual = json_run_query(PATH, "determinism: NonDeterminismCom");
+    fn determinism_test() {
+        let actual = json_run_query(PATH, "determinism: NonDeterministic1");
         assert!(matches!(
             actual,
             QueryResult::Determinism(DeterminismResult::Failure(., "1"))
         ))
     }
 
+    #[test]
+    fn not_consistent_from_test() {
+        let actual: QueryResult = json_run_query(PATH, "consistency: notConsistent");
+        assert!(matches!(
+            actual,
+            QueryResult::Consistency(ConsistencyResult::Failure(ConsistencyFailure::NotConsistentFrom(.,"")))
+        ))
+    }
 
+    #[test]
+    fn refinement_determinism_test() {
+        let actual: QueryResult =
+            json_run_query(PATH, "refinement: NonDeterministic1 <= NonDeterministic2");
+        assert!(matches!(
+            acutal,
+            QueryResult::Refinement(RefinementResult::Failure(
+                RefinementFailure::DeterminismFailure(.,"1")))
+        ))
+    }
+
+    #[test]
+    fn refinement_consistency_test() {
+        let actual: QueryResult = json_run_query(PATH, "");
+        assert!(matches!(
+            actual,
+            QueryResult::Refinement(RefinementResult::ConsistencyFailure(.,""))
+        ))
+    }
 }
