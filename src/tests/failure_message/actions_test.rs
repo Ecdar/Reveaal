69416263fba0c45b3562a5a1fb7c525ae0c70289
claes <claesm1999@gmail.com>
2022-11-16 13:01:28 +0100

    Updatet components and added tests

--- src/tests/failure_message/actions_test.rs
+++ src/tests/failure_message/actions_test.rs
@@ -1,7 +1,7 @@
 #[cfg(test)]
 
 mod test {
-    use crate::queries;
+
     use crate::tests::refinement::Helper::json_run_query;
     use crate::System::{
         executable_query::QueryResult,
@@ -13,39 +13,44 @@ mod test {
 
     #[test]
     fn determinism_test() {
-        let actual = json_run_query(PATH, "determinism: NonDeterministic1");
-        assert!(matches!(
-            actual,
-            QueryResult::Determinism(DeterminismResult::Failure(., "1"))
-        ))
+        let expected_action = String::from("1");
+        if let QueryResult::Determinism(DeterminismResult::Failure(_, actual_action)) =
+            json_run_query(PATH, "determinism: NonDeterministic1")
+        {
+            assert_eq!(expected_action, actual_action);
+        }
     }
 
     #[test]
     fn not_consistent_from_test() {
-        let actual: QueryResult = json_run_query(PATH, "consistency: notConsistent");
-        assert!(matches!(
-            actual,
-            QueryResult::Consistency(ConsistencyResult::Failure(ConsistencyFailure::NotConsistentFrom(.,"")))
-        ))
+        let expected_action = String::from("");
+        if let QueryResult::Consistency(ConsistencyResult::Failure(
+            ConsistencyFailure::NotConsistentFrom(_, actual_action),
+        )) = json_run_query(PATH, "consistency: NonConsistent")
+        {
+            assert_eq!(expected_action, actual_action);
+        }
     }
 
     #[test]
     fn refinement_determinism_test() {
-        let actual: QueryResult =
-            json_run_query(PATH, "refinement: NonDeterministic1 <= NonDeterministic2");
-        assert!(matches!(
-            acutal,
-            QueryResult::Refinement(RefinementResult::Failure(
-                RefinementFailure::DeterminismFailure(.,"1")))
-        ))
+        let expected_action = String::from("1");
+        if let QueryResult::Refinement(RefinementResult::Failure(
+            RefinementFailure::DeterminismFailure(_, actual_action),
+        )) = json_run_query(PATH, "refinement: NonDeterministic2 <= NonDeterministic2")
+        {
+            assert_eq!(expected_action, actual_action.unwrap());
+        }
     }
 
     #[test]
     fn refinement_consistency_test() {
-        let actual: QueryResult = json_run_query(PATH, "");
-        assert!(matches!(
-            actual,
-            QueryResult::Refinement(RefinementResult::ConsistencyFailure(.,""))
-        ))
+        let expected_action = String::from("");
+        if let QueryResult::Refinement(RefinementResult::Failure(
+            RefinementFailure::ConsistencyFailure(_, actual_action),
+        )) = json_run_query(PATH, "refinement: NonConsistent <= CorrectComponent")
+        {
+            assert_eq!(expected_action, actual_action.unwrap());
+        }
     }
 }
