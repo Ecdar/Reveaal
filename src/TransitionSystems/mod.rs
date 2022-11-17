a2e445f90b6f65e98fd486d987541a9b39921d82
Mati-AAU <74661610+Mati-AAU@users.noreply.github.com>
2022-11-17 12:21:47 +0100

    Group17 edge and transition ids generation (#92)
    Adding IDs to both the Edge struct and Transition struct.
    
    Edge IDs read directly from the JSON.
    XML is not supported in this pull request, neither the ECDAR GUI or Reveaal uses any XML examples anyway.
    
    Edge IDs are a unique string, we ensure uniqueness when saving to a component by remaking them when saved.
    
    Transition IDs are purely used to figure out which EdgeID a given transition consists of (kind of like LocationID, which the implementation is based on)
    
    Dont worry, 43 of the changed files is fixing old data. Old data did not generate edge IDs, now all the used test data has edge IDs.

--- src/TransitionSystems/mod.rs
+++ src/TransitionSystems/mod.rs
@@ -6,6 +6,7 @@ mod conjunction;
 mod location_id;
 mod location_tuple;
 mod quotient;
+mod transition_id;
 pub mod transition_system;
 
 pub use compiled_component::CompiledComponent;
@@ -14,4 +15,5 @@ pub use conjunction::Conjunction;
 pub use location_id::LocationID;
 pub use location_tuple::{CompositionType, LocationTuple};
 pub use quotient::Quotient;
+pub use transition_id::TransitionID;
 pub use transition_system::{TransitionSystem, TransitionSystemPtr};
