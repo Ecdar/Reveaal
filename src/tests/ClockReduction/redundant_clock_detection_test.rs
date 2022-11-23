9e7545de284c21aa1e938774650a367c11212e56
Simon Laursen <69715502+Laursen79@users.noreply.github.com>
2022-11-23 15:11:10 +0100

    Fixed clock reduction tests in the simple cases

--- src/tests/ClockReduction/redundant_clock_detection_test.rs
+++ src/tests/ClockReduction/redundant_clock_detection_test.rs
@@ -1,35 +1,52 @@
 #[cfg(test)]
 pub mod test {
-    use crate::tests::ClockReduction::helper::test::{assert_clock_reason, assert_unused_clock_in_clock_reduction_instruction_vec, compile_json_component, read_json_component_and_process};
+    use crate::tests::ClockReduction::helper::test::{
+        assert_clock_reason, assert_duplicate_clock_in_clock_reduction_instruction_vec,
+        assert_unused_clock_in_clock_reduction_instruction_vec, compile_json_component,
+        read_json_component_and_process,
+    };
     use crate::DataReader::json_reader::read_json_component;
+    use crate::TransitionSystems::transition_system::Heights;
     use crate::TransitionSystems::{CompiledComponent, TransitionSystem};
     use edbm::util::constraints::ClockIndex;
     use std::collections::{HashMap, HashSet};
     use test_case::test_case;
-    use crate::TransitionSystems::transition_system::Heights;
 
     const REDUNDANT_CLOCKS_TEST_PROJECT: &str = "samples/json/ClockReductionTest/RedundantClocks";
     const DIM: ClockIndex = 5; // TODO: Dim
 
     #[test]
     fn test_three_synced_clocks() {
-        let expected_clock:String = "x".to_string();
-        let component = read_json_component_and_process(REDUNDANT_CLOCKS_TEST_PROJECT, "Component1");
-        let compiled_component = CompiledComponent::compile(
-            component.clone(),
-            DIM,
-        )
-        .unwrap();
+        let expected_clocks = ["x".to_string(), "y".to_string(), "z".to_string()];
+        let component =
+            read_json_component_and_process(REDUNDANT_CLOCKS_TEST_PROJECT, "Component1");
+        let compiled_component = CompiledComponent::compile(component.clone(), DIM).unwrap();
         let instructions = compiled_component.find_redundant_clocks(Heights::empty());
-        let clock_index = component.declarations.get_clock_index_by_name(&expected_clock).unwrap();
+        let clock_index_x = component
+            .declarations
+            .get_clock_index_by_name(&expected_clocks[0])
+            .unwrap();
+        let clock_index_y = component
+            .declarations
+            .get_clock_index_by_name(&expected_clocks[1])
+            .unwrap();
+        let clock_index_z = component
+            .declarations
+            .get_clock_index_by_name(&expected_clocks[2])
+            .unwrap();
 
-        assert_unused_clock_in_clock_reduction_instruction_vec(instructions, *clock_index);
+        assert_duplicate_clock_in_clock_reduction_instruction_vec(
+            instructions.clone(),
+            *clock_index_x,
+            &HashSet::from([*clock_index_y, *clock_index_z]),
+        );
     }
 
     //TODO: This is not a valid test anymore
     #[test]
     fn test_three_synced_clocks_correct_location_target() {
-        let compiled_component = compile_json_component(REDUNDANT_CLOCKS_TEST_PROJECT, "Component1");
+        let compiled_component =
+            compile_json_component(REDUNDANT_CLOCKS_TEST_PROJECT, "Component1");
 
         let mut expected_locations: HashMap<String, HashSet<String>> = HashMap::new();
 
