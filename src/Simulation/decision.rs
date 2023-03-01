81fc37804a81bdceede5c28d1d17ba69a20e2e93
Sebastian Lund <sebastianlund112@gmail.com>
2023-03-01 23:33:28 +0100

    Change Decision to be a state and action. Removing the need for DecisionPoint and its 3 variants

--- src/Simulation/decision.rs
+++ src/Simulation/decision.rs
@@ -1,38 +1,69 @@
 use crate::{
-    component::{Edge, State},
+    component::{State, Transition},
     TransitionSystems::TransitionSystemPtr,
 };
 
-use super::{decision_point::DecisionPoint, transition_decision::TransitionDecision};
-
-/// Represent a decision in a any composition of components, that has been taken: In the current `source` state I have `decided` to use this [`Edge`].
-#[derive(Debug)]
+/// Represent a decision in a any composition of components: In the current `state` [`State`] we have decided to take this `action` [`String`].
+#[derive(Debug, Clone)]
 pub struct Decision {
-    source: State,
-    decided: Edge,
+    pub state: State,
+    pub action: String,
+    pub transition: Option<Transition>,
 }
 
 impl Decision {
-    pub fn new(source: State, decided: Edge) -> Self {
-        Self { source, decided }
-    }
-
-    pub fn source(&self) -> &State {
-        &self.source
-    }
-
-    pub fn decided(&self) -> &Edge {
-        &self.decided
-    }
-
-    /// Resolves a [`Decision`]: use the `decided` [`Edge`] and returns a [`Vec`] of the [`DecisionPoint`]s of the destination [`State`]s.
+    /// Resolves a [`Decision`]: use the `action` in the `state` and return a [`Vec`] of the possible [`Decision`]s from the destination [`State`].
     ///
-    /// Some `decided` [`Edge`]s lead to ambiguity ie. they correspond to multiple [`Transition`]s. Thus one [`Edge`] can lead to multiple [`State`]s.  
-    pub fn resolve(&self, system: &TransitionSystemPtr) -> Vec<DecisionPoint> {
-        TransitionDecision::from(self, system)
+    /// # Panics
+    /// Panics if the [`Decision`] leads to no new states or is ambiguous (leads to multiple new states)
+    pub fn resolve(&self, system: &TransitionSystemPtr) -> Vec<Decision> {
+        let transitions = system.next_transitions(&self.state.decorated_locations, &self.action);
+        let mut next_states: Vec<_> = transitions
             .into_iter()
-            .filter_map(|transition_decision| transition_decision.resolve(system))
-            .map(|transition_decision_point| DecisionPoint::from(&transition_decision_point))
-            .collect()
+            .filter_map(|transition| transition.use_transition_alt(&self.state))
+            .collect();
+
+        assert_ne!(next_states.len(), 0, "Decision leads to no new states");
+        assert_eq!(
+            next_states.len(),
+            1,
+            "Ambiguous decision leads to multiple new states"
+        );
+
+        let next_state = next_states.pop().unwrap();
+
+        Decision::get_decisions_from_state(next_state, system)
+    }
+
+    /// Get all possible [`Decision`]s from a [`State`]
+    pub fn get_decisions_from_state(state: State, system: &TransitionSystemPtr) -> Vec<Decision> {
+        let mut next_decisions = vec![];
+
+        for action in system.get_actions() {
+            let possible_transitions = system.next_transitions(&state.decorated_locations, &action);
+            for t in possible_transitions {
+                let allowed = t.get_allowed_federation();
+                let intersection = allowed.intersection(state.zone_ref());
+                if !intersection.is_empty() {
+                    next_decisions.push(Decision {
+                        state: State::create(state.decorated_locations.clone(), intersection),
+                        action: action.clone(),
+                        transition: Some(t),
+                    });
+                }
+            }
+        }
+
+        next_decisions
+    }
+
+    /// Get all possible [`Decision`]s from the initial state of a [`TransitionSystemPtr`]
+    pub fn get_initial_decisions(system: &TransitionSystemPtr) -> Vec<Decision> {
+        Decision::get_decisions_from_state(
+            system
+                .get_initial_state()
+                .expect("Expected system to have initial state"),
+            system,
+        )
     }
 }
