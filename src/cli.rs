9a1ad09064e8f0f955f0d91e753caadf9b38b070
Sebastian Lund <sebastianlund112@gmail.com>
2023-03-30 18:33:34 +0200

    Cargo fmt

--- src/cli.rs
+++ src/cli.rs
@@ -7,11 +7,11 @@ use std::path::PathBuf;
     long_about = Some("With Reveaal you can either run a single query with the 'query' command or run it as a server with the 'serve' command"))]
 pub enum Args {
     /// Start a gRPC server with the protocol defined in the protobuf file
-    /// 
+    ///
     /// Examples of usage:
-    /// 
+    ///
     /// Reveaal serve 127.0.0.1:4242
-    /// 
+    ///
     /// Reveaal serve -t 1 -c 50 127.0.0.1:4242
     Serve {
         /// Ip address and port to serve the gRPC server on
@@ -27,13 +27,13 @@ pub enum Args {
         cache_size: usize,
     },
     /// Run a query
-    /// 
+    ///
     /// Examples of usage:
-    /// 
+    ///
     /// Reveaal query "refinement: Researcher || Machine || Administration <= Spec" -i samples/json/EcdarUniversity
-    /// 
+    ///
     /// Reveaal query "consistency: Machine" -i samples/json/EcdarUniversity
-    /// 
+    ///
     /// Reveaal query "determinism: Researcher" -i samples/json/EcdarUniversity
     Query {
         /// The query to execute
