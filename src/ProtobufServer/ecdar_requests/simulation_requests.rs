c481d9c90bc57ea0467dc07444cc0d38bd978b68
Sebastian Lund <sebastianlund112@gmail.com>
2023-05-02 13:48:01 +0200

    Move simulation start and step to a shared file

--- src/ProtobufServer/ecdar_requests/simulation_requests.rs
+++ src/ProtobufServer/ecdar_requests/simulation_requests.rs
@@ -6,13 +6,35 @@ use crate::{
         proto_reader::{proto_decision_to_decision, simulation_info_to_transition_system},
     },
     ProtobufServer::{
-        services::{SimulationStepRequest, SimulationStepResponse},
+        services::{SimulationStartRequest, SimulationStepRequest, SimulationStepResponse},
         ConcreteEcdarBackend,
     },
     System::specifics::SpecificDecision,
 };
 
+use crate::Simulation::decision::Decision;
+
 impl ConcreteEcdarBackend {
+    /// Handles a start simulation request: Responding with the initial decision point in the transition system given in the `request`.
+    pub fn handle_start_simulation(
+        request: SimulationStartRequest,
+        _cache: ModelCache, // TODO should be used...
+    ) -> Result<SimulationStepResponse, Status> {
+        let simulation_info = request.simulation_info.unwrap();
+
+        let transition_system = simulation_info_to_transition_system(&simulation_info);
+
+        // Get the decisions from the initial state and convert them to proto
+        let initial = Decision::get_initial_decisions(&transition_system)
+            .into_iter()
+            .map(|i| SpecificDecision::from_decision(&i, &*transition_system).into())
+            .collect();
+
+        Ok(SimulationStepResponse {
+            new_decision_points: initial,
+        })
+    }
+
     /// Handles a take simulation step request:
     /// Given a `decision` and transition system in the `request`, walk along the decided edge and respond with the resulting decision points.
     pub fn handle_take_simulation_step(
