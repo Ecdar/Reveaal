b7fa43b3ea910b281e049c744de38c9ae907ef12
Sebastian Lund <sebastianlund112@gmail.com>
2023-05-11 22:02:40 +0200

    Add lazy_static crate for cleaner parser code

--- src/DataReader/parse_queries.rs
+++ src/DataReader/parse_queries.rs
@@ -13,16 +13,20 @@ use pest::Parser;
 #[grammar = "DataReader/grammars/query_grammar.pest"]
 pub struct QueryParser;
 
+lazy_static! {
+    static ref PRATT: PrattParser<Rule> = PrattParser::new()
+        .op(Op::infix(Rule::qoutient_op, Assoc::Left))
+        .op(Op::infix(Rule::composition_op, Assoc::Left))
+        .op(Op::infix(Rule::conjunction_op, Assoc::Left));
+}
+
 ///This file handles parsing the queries based on the abstract syntax described in the .pest files in the grammar folder
 ///For clarification see documentation on pest crate
 
-pub fn parse_system(
-    pair: pest::iterators::Pair<Rule>,
-    pratt: &PrattParser<Rule>,
-) -> SystemExpression {
-    pratt
+pub fn parse_system(pair: pest::iterators::Pair<Rule>) -> SystemExpression {
+    PRATT
         .map_primary(|pair| match pair.as_rule() {
-            Rule::expr => parse_system(pair, pratt),
+            Rule::expr => parse_system(pair),
             Rule::component => {
                 let mut pairs = pair.into_inner();
                 let comp_name = pairs.next().unwrap().as_str().to_string();
@@ -75,8 +79,8 @@ pub fn parse_operand(pair: pest::iterators::Pair<Rule>) -> OperandExpression {
     }
 }
 
-fn parse_state(pair: pest::iterators::Pair<Rule>, pratt: &PrattParser<Rule>) -> StateExpression {
-    pratt
+fn parse_state(pair: pest::iterators::Pair<Rule>) -> StateExpression {
+    PRATT
         .map_primary(|pair| match pair.as_rule() {
             Rule::andExpr | Rule::orExpr => {
                 let rule = pair.as_rule();
@@ -84,22 +88,17 @@ fn parse_state(pair: pest::iterators::Pair<Rule>, pratt: &PrattParser<Rule>) -> 
                 let mut inner = pair.into_inner();
                 let len = inner.clone().count();
                 if len == 1 {
-                    return parse_state(inner.next().unwrap(), pratt);
+                    return parse_state(inner.next().unwrap());
                 }
                 match rule {
-                    Rule::andExpr => {
-                        StateExpression::AND(inner.map(|pair| parse_state(pair, pratt)).collect())
-                    }
-                    Rule::orExpr => {
-                        StateExpression::OR(inner.map(|pair| parse_state(pair, pratt)).collect())
-                    }
+                    Rule::andExpr => StateExpression::AND(inner.map(parse_state).collect()),
+                    Rule::orExpr => StateExpression::OR(inner.map(parse_state).collect()),
                     _ => unreachable!(),
                 }
             }
-            Rule::notExpr => StateExpression::NOT(Box::new(parse_state(
-                pair.into_inner().next().unwrap(),
-                pratt,
-            ))),
+            Rule::notExpr => {
+                StateExpression::NOT(Box::new(parse_state(pair.into_inner().next().unwrap())))
+            }
             Rule::compExpr => {
                 let mut pairs = pair.into_inner();
                 let first = pairs.next().unwrap();
@@ -141,59 +140,59 @@ fn comp_var_from_variable_pair(pair: pest::iterators::Pair<Rule>) -> ComponentVa
     }
 }
 
-fn parse_query(pair: pest::iterators::Pair<Rule>, pratt: &PrattParser<Rule>) -> QueryExpression {
-    pratt
+fn parse_query(pair: pest::iterators::Pair<Rule>) -> QueryExpression {
+    PRATT
         .map_primary(|pair| {
             let query = match pair.as_rule() {
                 Rule::refinement => {
                     let mut pairs = pair.into_inner();
-                    let system1 = parse_system(pairs.next().unwrap(), pratt);
-                    let system2 = parse_system(pairs.next().unwrap(), pratt);
+                    let system1 = parse_system(pairs.next().unwrap());
+                    let system2 = parse_system(pairs.next().unwrap());
                     QueryExpression::Refinement(system1, system2)
                 }
                 Rule::consistency => {
                     let mut pairs = pair.into_inner();
-                    let system = parse_system(pairs.next().unwrap(), pratt);
+                    let system = parse_system(pairs.next().unwrap());
                     QueryExpression::Consistency(system)
                 }
                 Rule::reachability => {
                     let mut pairs = pair.into_inner();
-                    let system = parse_system(pairs.next().unwrap(), pratt);
-                    let to = parse_state(pairs.next_back().unwrap(), pratt);
-                    let from = pairs.next().map(|pair| parse_state(pair, pratt));
+                    let system = parse_system(pairs.next().unwrap());
+                    let to = parse_state(pairs.next_back().unwrap());
+                    let from = pairs.next().map(parse_state);
 
                     QueryExpression::Reachability { system, from, to }
                 }
                 Rule::implementation => {
                     let mut pairs = pair.into_inner();
-                    let system = parse_system(pairs.next().unwrap(), pratt);
+                    let system = parse_system(pairs.next().unwrap());
                     QueryExpression::Implementation(system)
                 }
                 Rule::determinism => {
                     let mut pairs = pair.into_inner();
-                    let system = parse_system(pairs.next().unwrap(), pratt);
+                    let system = parse_system(pairs.next().unwrap());
                     QueryExpression::Determinism(system)
                 }
                 Rule::specification => {
                     let mut pairs = pair.into_inner();
-                    let system = parse_system(pairs.next().unwrap(), pratt);
+                    let system = parse_system(pairs.next().unwrap());
                     QueryExpression::Specification(system)
                 }
                 Rule::getComponent => {
                     let mut pairs = pair.into_inner();
-                    let system = parse_system(pairs.next().unwrap(), pratt);
+                    let system = parse_system(pairs.next().unwrap());
                     let name = pairs.next().map(|it| it.as_str().to_string());
                     QueryExpression::GetComponent(SaveExpression { system, name })
                 }
                 Rule::prune => {
                     let mut pairs = pair.into_inner();
-                    let system = parse_system(pairs.next().unwrap(), pratt);
+                    let system = parse_system(pairs.next().unwrap());
                     let name = pairs.next().map(|it| it.as_str().to_string());
                     QueryExpression::Prune(SaveExpression { system, name })
                 }
                 Rule::bisim => {
                     let mut pairs = pair.into_inner();
-                    let system = parse_system(pairs.next().unwrap(), pratt);
+                    let system = parse_system(pairs.next().unwrap());
                     let name = pairs.next().map(|it| it.as_str().to_string());
                     QueryExpression::BisimMinim(SaveExpression { system, name })
                 }
@@ -204,14 +203,11 @@ fn parse_query(pair: pest::iterators::Pair<Rule>, pratt: &PrattParser<Rule>) -> 
         .parse(pair.into_inner())
 }
 
-fn parse_queries(
-    pair: pest::iterators::Pair<Rule>,
-    pratt: &PrattParser<Rule>,
-) -> Vec<QueryExpression> {
+fn parse_queries(pair: pest::iterators::Pair<Rule>) -> Vec<QueryExpression> {
     match pair.as_rule() {
         Rule::queryList => {
             let inner = pair.into_inner();
-            inner.map(|pair| parse_query(pair, pratt)).collect()
+            inner.map(parse_query).collect()
         }
         _ => unreachable!("Unexpected rule: {:?}", pair.as_rule()),
     }
@@ -235,49 +231,34 @@ pub fn parse_to_query(input: &str) -> Vec<Query> {
 }
 
 pub fn parse_to_expression_tree(input: &str) -> Result<Vec<QueryExpression>, String> {
-    let pratt = PrattParser::new()
-        .op(Op::infix(Rule::qoutient_op, Assoc::Left))
-        .op(Op::infix(Rule::composition_op, Assoc::Left))
-        .op(Op::infix(Rule::conjunction_op, Assoc::Left));
-
     let mut pairs = match QueryParser::parse(Rule::queries, input) {
         Ok(pairs) => pairs,
         Err(e) => return Err(format!("Could not parse as rule with error: {}", e)),
     };
 
-    let result = parse_queries(pairs.next().unwrap(), &pratt);
+    let result = parse_queries(pairs.next().unwrap());
 
     Ok(result)
 }
 
 pub fn parse_to_system_expr(input: &str) -> Result<SystemExpression, String> {
-    let pratt = PrattParser::new()
-        .op(Op::infix(Rule::qoutient_op, Assoc::Left))
-        .op(Op::infix(Rule::composition_op, Assoc::Left))
-        .op(Op::infix(Rule::conjunction_op, Assoc::Left));
-
     let mut pairs = match QueryParser::parse(Rule::expr, input) {
         Ok(pairs) => pairs,
         Err(e) => return Err(format!("Could not parse as rule with error: {}", e)),
     };
 
-    let result = parse_system(pairs.next().unwrap(), &pratt);
+    let result = parse_system(pairs.next().unwrap());
 
     Ok(result)
 }
 
 pub fn parse_to_state_expr(input: &str) -> Result<StateExpression, String> {
-    let pratt = PrattParser::new()
-        .op(Op::infix(Rule::qoutient_op, Assoc::Left))
-        .op(Op::infix(Rule::composition_op, Assoc::Left))
-        .op(Op::infix(Rule::conjunction_op, Assoc::Left));
-
     let mut pairs = match QueryParser::parse(Rule::state, input) {
         Ok(pairs) => pairs,
         Err(e) => return Err(format!("Could not parse as rule with error: {}", e)),
     };
 
-    let result = parse_state(pairs.next().unwrap(), &pratt);
+    let result = parse_state(pairs.next().unwrap());
 
     Ok(result)
 }
