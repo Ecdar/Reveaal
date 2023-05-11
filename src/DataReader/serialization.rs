fe9bf6df1d9d8b42949a88de51a55d722d6c6507
Sebastian Lund <sebastianlund112@gmail.com>
2023-05-11 22:04:08 +0200

    Combine invariant and edge grammar, and refactor parse_edge code to be more readable and compact

--- src/DataReader/serialization.rs
+++ src/DataReader/serialization.rs
@@ -1,5 +1,4 @@
 use crate::DataReader::parse_edge;
-use crate::DataReader::parse_invariant;
 use crate::ModelObjects::component::{
     Component, Declarations, Edge, Location, LocationType, SyncType,
 };
@@ -241,15 +240,9 @@ where
     if s.is_empty() {
         return Ok(None);
     }
-    match parse_edge::parse(&s) {
-        Ok(edgeAttribute) => match edgeAttribute {
-            parse_edge::EdgeAttribute::Guard(guard_res) => Ok(Some(guard_res)),
-            parse_edge::EdgeAttribute::Updates(_) => {
-                panic!("We expected a guard but got an update? {:?}\n", s)
-            }
-        },
-        Err(e) => panic!("Could not parse {} got error: {:?}", s, e),
-    }
+    parse_edge::parse_guard(&s).map(Some).map_err(|err| {
+        serde::de::Error::custom(format!("Could not parse {} got error: {:?}", s, err))
+    })
 }
 
 //Function used for deserializing updates
@@ -261,15 +254,10 @@ where
     if s.is_empty() {
         return Ok(None);
     }
-    match parse_edge::parse(&s) {
-        Ok(edgeAttribute) => match edgeAttribute {
-            parse_edge::EdgeAttribute::Guard(_) => {
-                panic!("We expected an update but got a guard? {:?}", s)
-            }
-            parse_edge::EdgeAttribute::Updates(update_vec) => Ok(Some(update_vec)),
-        },
-        Err(e) => panic!("Could not parse {} got error: {:?}", s, e),
-    }
+
+    parse_edge::parse_updates(&s).map(Some).map_err(|err| {
+        serde::de::Error::custom(format!("Could not parse {} got error: {:?}", s, err))
+    })
 }
 
 //Function used for deserializing invariants
@@ -283,7 +271,7 @@ where
     if s.is_empty() {
         return Ok(None);
     }
-    match parse_invariant::parse(&s) {
+    match parse_edge::parse_guard(&s) {
         Ok(edgeAttribute) => Ok(Some(edgeAttribute)),
         Err(e) => panic!("Could not parse invariant {} got error: {:?}", s, e),
     }
