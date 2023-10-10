0e8c64912c06d6ca1f4ec51ec6c4e8023358166e
Rasmus Hald Nielsen <Rasmus.hald.nielsen@outlook.dk>
2023-10-10 11:24:13 +0200

    Component cache unit tests
    Tests for when a component exists in cache and when it does not.

--- src/tests/grpc/send_query.rs
+++ src/tests/grpc/send_query.rs
@@ -58,6 +58,76 @@ mod refinements {
         }
     }
 
+    #[tokio::test]
+    async fn send_query_using_cache() {
+        let backend = ConcreteEcdarBackend::default();
+        let query_request = construct_query_request_for_cache("refinement: Machine <= Machine");
+
+        // Normal query request, including component.
+        let query_response = backend.send_query(query_request.0).await;
+
+        let query_result = query_response.unwrap().into_inner();
+        let normal_result = query_result.result.unwrap();
+
+        // Component should be cached now.
+        // Query without component utilizing cache
+        let query_response = backend.send_query(query_request.1).await;
+
+        let query_result = query_response.unwrap().into_inner();
+        let cache_result = query_result.result.unwrap();
+
+        // Compare normal and cache response.
+        assert_eq!(normal_result, cache_result);
+    }
+
+    #[tokio::test]
+    async fn send_query_not_in_cache() {
+        let backend = ConcreteEcdarBackend::default();
+        let query_request = construct_query_request_for_cache("refinement: Machine <= Machine");
+
+        // Cache request
+        let query_response = backend.send_query(query_request.1).await;
+
+        let query_result = query_response.unwrap().into_inner();
+        let result = query_result.result.unwrap();
+        match result {
+            query_response::Result::ComponentsNotInCache(_) => {},
+            _ => panic!("Expected failure, got {:?}", result),
+        }
+    }
+
+    fn construct_query_request_for_cache(
+        query: &str,
+    ) -> (Request<QueryRequest>, Request<QueryRequest>) {
+        let json =
+            std::fs::read_to_string(format!("{}/Components/Machine.json", ECDAR_UNI)).unwrap();
+
+        let normal_request = Request::new(QueryRequest {
+            user_id: 0,
+            query_id: 0,
+            query: String::from(query),
+            components_info: Some(ComponentsInfo {
+                components: vec![Component {
+                    rep: Some(Rep::Json(json)),
+                }],
+                components_hash: 1,
+            }),
+            settings: Some(crate::tests::TEST_SETTINGS),
+        });
+
+        let cached_request = Request::new(QueryRequest {
+            user_id: 0,
+            query_id: 0,
+            query: String::from(query),
+            components_info: Some(ComponentsInfo {
+                components: vec![],
+                components_hash: 1,
+            }),
+            settings: Some(crate::tests::TEST_SETTINGS),
+        });
+        (normal_request, cached_request)
+    }
+
     fn construct_query_request(query: &str) -> Request<QueryRequest> {
         let json =
             std::fs::read_to_string(format!("{}/Components/Machine.json", ECDAR_UNI)).unwrap();
@@ -108,3 +178,4 @@ mod refinements {
         }
     }
 }
+
